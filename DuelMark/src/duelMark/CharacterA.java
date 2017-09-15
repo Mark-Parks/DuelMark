579bc9a4b92efefba6f886e92f2fe36ea240c193
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2017-09-15 15:30:04 -0400

    charactera

--- DuelMark/src/duelMark/CharacterA.java
+++ DuelMark/src/duelMark/CharacterA.java
@@ -3,5 +3,30 @@ package duelMark;
 public class CharacterA implements Dueler
 {
 	public void taunt(){};
-	public String getName() {};
+	public String getName() {}
+	@Override
+	public void setStartingHP(int hp) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public int getHP() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public int getAction(Object caller) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void hit(Object caller) {
+		// TODO Auto-generated method stub
+		
+	}
 }
