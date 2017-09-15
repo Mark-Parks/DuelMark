80aaad5ff3fa2afdd34caa903f2c0a40974a77b3
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2017-09-15 15:16:52 -0400

    Updated Duel / Dueler

--- DuelMark/src/duel/CharacterB.java
+++ DuelMark/src/duel/CharacterB.java
@@ -8,4 +8,33 @@ public class CharacterB implements Dueler
 	public String getName(String name){
 		return name;
 	}
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setStartingHP(int hp) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public int getHP() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public int getAction(Object caller) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void hit(Object caller) {
+		// TODO Auto-generated method stub
+		
+	}
 }
