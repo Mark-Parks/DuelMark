d38154fee9578f8b1e4095a154d53effaaa913e6
wwu5040 <31863372+wwu5040@users.noreply.github.com>
2017-09-25 15:31:45 -0400

    Update CharacterB.java

--- DuelMark/src/duelMark/CharacterB.java
+++ DuelMark/src/duelMark/CharacterB.java
@@ -3,6 +3,9 @@ package duelMark;
 public class CharacterB implements Dueler
 {
 	private int hp;
+	private boolean enemyLoaded;
+	private boolean selfLoaded;
+	private Dueler d2;
 	
 	public void taunt(){
 		System.out.println("It's high noon...");
@@ -12,23 +15,50 @@ public class CharacterB implements Dueler
 	}
 	public void setStartingHP(int hp) {
 		this.hp = hp;
+		enemyLoaded = false;
+		selfLoaded = true;
 	}
 	public int getHP() {
 		return hp;
 	}
 	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
-		return (d.hp > hp);
+		d2 = d;
+		return (d.getHP() == hp);
 	}
 	public int getAction(Object caller) {
 		//return the action chosen for this round
 		//Object is the object calling this method. Use it to verify your opponent is not trying to gain the upperhand by figuring out what you are doing.
-		//no idea how to do this
-		return 0;
+		if(caller instanceof Duel) {
+			
+			Duel duel = (Duel)caller;
+		
+			if(duel.getLastActionOf(d2) == duel.LOADING)
+			{
+				enemyLoaded = true;
+			}
+			if(duel.getLastActionOf(d2) == duel.SHOOTING)
+			{
+				enemyLoaded = false;
+			}
+			if(enemyLoaded == false && selfLoaded == false){
+				selfLoaded = true;
+				return duel.LOADING;
+			}
+			else if(selfLoaded == true){
+				selfLoaded = false;
+				return duel.SHOOTING;
+			}
+			else
+			{
+				return duel.GUARDING;
+			}
+		}
+		return 3;
 	}
 	public void hit(Object caller) {
 		//likely wrong
 		if (caller instanceof Duel) {
-			this.hp -= 10;
+			hp -= 10;
 		}
 	}
 }
